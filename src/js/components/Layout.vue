e6c3776c17a25b31c61172df3c775af145d061a6
Vitor Leonel <falecom@vitorleonel.com>
2018-02-13 03:06:30 -0200

    layout após logado

---
+++ src/js/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav class="navbar is-white has-shadow">
+            <div class="container">
+
+                <div class="navbar-brand">
+                    <div class="navbar-item">
+                        <a href="" class="title is-5">snaploss</a>
+                    </div>
+                </div>
+
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <div class="navbar-item">
+                            <a href="" class="button is-link is-outlined">Adicionar animal</a>
+                        </div>
+                    </div>
+
+                    <div class="navbar-end">
+                        <router-link :to="{ name: 'app.home.index' }" class="navbar-item" exact>Feed</router-link>
+                        <router-link :to="{ name: 'app.animals.index' }" class="navbar-item">Animais Perdidos</router-link>
+                        <router-link :to="{ name: 'app.account.index' }" class="navbar-item">Minha Conta</router-link>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+
+        <router-view></router-view>
+    </div>
+</template>
--- src/js/router/index.js
+++ src/js/router/index.js
@@ -8,10 +8,33 @@ const routes = [
         name: 'app.login.index',
         path: '/entrar',
         component: () => import('@scripts/pages/entrar/Entrar')
+    },
+    {
+        path: '/',
+        component: () => import('@scripts/components/Layout'),
+        children: [
+            {
+                name: 'app.home.index',
+                path: '/',
+                component: () => import('@scripts/pages/home/Home')
+            },
+            {
+                name: 'app.animals.index',
+                path: '/animais',
+                component: () => import('@scripts/pages/home/Home')
+            },
+            {
+                name: 'app.account.index',
+                path: '/minha-conta',
+                component: () => import('@scripts/pages/home/Home')
+            }
+        ]
     }
 ]
 
 const router = new VueRouter({
+    linkActiveClass: 'is-active',
+    linkExactActiveClass: 'is-active',
     routes
 })
 
