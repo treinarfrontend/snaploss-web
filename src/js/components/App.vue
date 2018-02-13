3a65582b19dc02deab4ff804802ffda30b0a04af
Vitor Leonel <falecom@vitorleonel.com>
2018-02-13 01:23:54 -0200

    componente principal

---
+++ src/js/components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet commodi consectetur debitis exercitationem explicabo, impedit incidunt iure laborum nemo officiis quae quam quisquam, sit totam veritatis voluptates voluptatibus?</div>
+</template>
+
+<style lang="scss">
+    @import "~@styles/app.scss";
+</style>
