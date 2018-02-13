023281499f18d6e01143a3a0a62767c7330abd05
Vitor Leonel <falecom@vitorleonel.com>
2018-02-13 01:22:54 -0200

    componente principal

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 
+import App from './components/App'
+
 new Vue({
     el: '#app',
+    render: h => h(App),
     router,
     store
 })
